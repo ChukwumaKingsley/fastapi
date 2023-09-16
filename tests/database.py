6b873df2639901c784ede6008bb216f4d22ddccd
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-09-16 11:19:07 +0100

    Added tests for votes. Last test not working tho

--- tests/database.py
+++ tests/database.py
@@ -6,7 +6,6 @@ from app.main import app
 from app.config import settings
 from app.database import get_db, Base
 
-
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
