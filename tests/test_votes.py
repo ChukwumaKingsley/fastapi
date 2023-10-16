8f12dbb8e196023e7e52ec4576c9d0719f3a0924
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-16 14:57:18 +0100

    Commented out votes tests

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -2,16 +2,16 @@ import pytest
 from app import models
 
 
-@pytest.fixture()
-def test_vote(test_post, session, test_user):
-    new_vote = models.Vote(post_id=test_post[3].id, user_id=test_user['id'])
-    session.add(new_vote)
-    session.commit()
+# @pytest.fixture()
+# def test_vote(test_post, session, test_user):
+#     new_vote = models.Vote(post_id=test_post[3].id, user_id=test_user['id'])
+#     session.add(new_vote)
+#     session.commit()
 
-def test_vote_on_post(authorized_client, test_post):
-    res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 1})
-    assert res.status_code == 201
+# def test_vote_on_post(authorized_client, test_post):
+#     res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 1})
+#     assert res.status_code == 201
 
-def test_vote_twice_on_post(authorized_client, test_post, test_vote):
-    res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 1})
-    assert res.status_code == 409
+# def test_vote_twice_on_post(authorized_client, test_post, test_vote):
+#     res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 1})
+#     assert res.status_code == 409
